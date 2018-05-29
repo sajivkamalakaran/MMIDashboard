62b5538a19466aaffea45c5c2b5b7a1cc78affa8
sajivkamalakaran <sajiv.kamalakaran@cormac-corp.com>
2018-05-28 23:22:45 -0400

    Third Commit

--- js/bar.js
+++ js/bar.js
@@ -1,15 +1,15 @@
-d3.json("data/bar.json",function(data){
-    var enrolled = data["Enrolled"].map((d) => parseInt(d));
-    var target = data["Target"].map((d) => parseInt(d));
-    var dates = data["Date"]
-    var dt = {
-        date:dates,
-        enrolled:enrolled,
-        target:target
-    }
-    console.log(dt);
-    var bar = new BarChart("#bar",dt,"date","enrolled","Enrolled","time","linear")
-})
-// $("#bar-select").on("change",function(){
-//     bar1.wrangleData("name",$("#var-select").val())
+// d3.json("data/bar.json",function(data){
+//     var enrolled = data["Enrolled"].map((d) => parseInt(d));
+//     var target = data["Target"].map((d) => parseInt(d));
+//     var dates = data["Date"]
+//     var dt = {
+//         date:dates,
+//         enrolled:enrolled,
+//         target:target
+//     }
+//     console.log(dt);
+//     var bar = new BarChart("#bar",dt,"date","enrolled","Enrolled","time","linear")
 // })
+// // $("#bar-select").on("change",function(){
+// //     bar1.wrangleData("name",$("#var-select").val())
+// // })
